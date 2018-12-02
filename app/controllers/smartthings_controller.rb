4f4ee0c1bcb4f37a94926af1f67f424724b5a4a7
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-01 21:52:05 -0500

    putting url back together

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -6,7 +6,7 @@ class SmartthingsController < ApplicationController
     off_payload = {on: false}.to_json
     json_header = {:content_type => :json}
     # Thread.new do
-      RestClient.put(base_url + '/groups/4/action', off_payload, json_header)
+      RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/4/action', off_payload, json_header)
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/2/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/3/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: true, bri: 254})

0938cd5e5f50b3ff5397efa9201e5070e62e56c6
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-01 21:49:18 -0500

    testing variables to make things simpler

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -2,11 +2,11 @@ require 'rest_client'
 
 class SmartthingsController < ApplicationController
   def test
+    base_url = 'http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT'
+    off_payload = {on: false}.to_json
+    json_header = {:content_type => :json}
     # Thread.new do
-    #   json = '{"on": false}'
-      response = RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/4/action',
-                                {on: false}.to_json, {:content_type => :json})
-
+      RestClient.put(base_url + '/groups/4/action', off_payload, json_header)
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/2/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/3/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: true, bri: 254})

f5c24690b4bf0b25401148e25a1a084c433dde24
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-01 21:23:19 -0500

    sending through the response from hue

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -3,7 +3,8 @@ require 'rest_client'
 class SmartthingsController < ApplicationController
   def test
     # Thread.new do
-      RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/4/action', {on: false}, {:content_type => :json})
+      response = RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/4/action',
+                                {on: false}, {:content_type => :json})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/2/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/3/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: true, bri: 254})
@@ -11,6 +12,7 @@ class SmartthingsController < ApplicationController
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: false})
     # end
 
-    render :json => {message: 'This is a return message'}
+    # render :json => {message: 'This is a return message'}
+    render response
   end
 end

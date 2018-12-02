5ca5e112e9632087781dad35143aab89041e80a5
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-01 21:35:23 -0500

    making json into string

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -3,8 +3,10 @@ require 'rest_client'
 class SmartthingsController < ApplicationController
   def test
     # Thread.new do
+      json = '{"on": false}'
       response = RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/4/action',
-                                {on: false}, {:content_type => :json})
+                                json, {:content_type => :json})
+
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/2/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/3/action', {on: false})
       # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: true, bri: 254})

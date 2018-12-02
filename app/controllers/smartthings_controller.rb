c41ac54a6cd990db996d7bd6ccfc220f1e96914f
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-01 21:59:33 -0500

    cleaning everything up and turning all lights and threading back on

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -5,16 +5,15 @@ class SmartthingsController < ApplicationController
     base_url = 'http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT'
     off_payload = {on: false}.to_json
     json_header = {:content_type => :json}
-    # Thread.new do
-      response = RestClient.put(base_url + '/groups/4/action', off_payload, json_header)
-      # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/2/action', {on: false})
-      # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/3/action', {on: false})
-      # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: true, bri: 254})
-      # sleep 300
-      # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: false})
-    # end
+    Thread.new do
+      RestClient.put(base_url + '/groups/4/action', off_payload, json_header)
+      RestClient.put(base_url + '/groups/2/action', off_payload, json_header)
+      RestClient.put(base_url + '/groups/3/action', off_payload, json_header)
+      RestClient.put(base_url + '/lights/10/state', {on: true, bri: 254}.to_json, json_header)
+      sleep 300
+      RestClient.put(base_url + '/lights/10/state', off_payload, json_header)
+    end
 
     # render :json => {message: 'This is a return message'}
-    render :json => response
   end
 end

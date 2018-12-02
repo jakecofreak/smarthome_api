ea60e17428b797cae415f89b813e4c6c793bf01b
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-01 20:23:35 -0500

    removing threading

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -2,14 +2,14 @@ require 'rest_client'
 
 class SmartthingsController < ApplicationController
   def test
-    Thread.new do
+    # Thread.new do
       RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/4/action', {on: false})
       RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/2/action', {on: false})
       RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/groups/3/action', {on: false})
-      RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: true, bri: 254})
-      sleep 300
-      RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: false})
-    end
+      # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: true, bri: 254})
+      # sleep 300
+      # RestClient.put('http://172.30.0.106/api/Bo8C0mvSaLK98DbrOkNqdu4c80779GvOsrk2rpuT/lights/10/state', {on: false})
+    # end
 
     render :json => {message: 'This is a return message'}
   end

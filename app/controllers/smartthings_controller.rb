3c7a12fae425050d8224645d213e598fe9c23906
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-09 12:01:48 -0500

    adding test endpoint that returns json

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -7,4 +7,15 @@ class SmartthingsController < ApplicationController
     logger.info log_request
     render :json => { message: "Logs written successfully." }
   end
+
+  def test_endpoint
+    render :json => { railsMessage: "This is a test json payload response.",
+                      key1: "value1",
+                      key2: "value2",
+                      key3: {
+                          nestedKey1: "nestedValue1",
+                          nestedKey2: "nestedValue2"
+                      },
+                      key4: "value4" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   post 'logs', to: 'smartthings#logs'
+  get 'test', to: 'smartthings#test_endpoint'
 end

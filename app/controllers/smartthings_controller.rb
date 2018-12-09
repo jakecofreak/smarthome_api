7cc2b81f6dc436352b7c3074595b2e215623290e
Andrew Marshall <andrewmarshall@bitbucket.org>
2018-12-09 09:44:04 -0500

    added log response

--- app/controllers/smartthings_controller.rb
+++ app/controllers/smartthings_controller.rb
@@ -9,6 +9,7 @@ class SmartthingsController < ApplicationController
   def logs
     log_request = ActiveSupport::JSON.decode(request.body.read).deep_symbolize_keys!
     logger.info log_request
+    render :json => { message: "Logs written successfully." }
   end
 
   def leaving

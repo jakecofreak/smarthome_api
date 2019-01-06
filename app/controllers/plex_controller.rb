7394e43191020653024e29e93cb08e90194390ed
Andrew Marshall <andrewmarshall@bitbucket.org>
2019-01-06 17:19:48 -0500

    custom plex notifications based on known IP addresses per user

---
+++ app/controllers/plex_controller.rb
@@ -0,0 +1,71 @@
+require 'rest-client'
+
+class PlexController < ApplicationController
+  def notify
+    # set vars and parse notification payload
+    payload = {}
+    headers = { :content_type => :json }
+    ifttt_url = 'https://maker.ifttt.com/trigger/plex/with/key/bd8RYS8JENBzOlYhHOzxMv'
+    notification = ActiveSupport::JSON.decode(request.body.read).deep_symbolize_keys!
+    user = notification[:user].partition('@').first.to_sym
+
+    # exit early if stream was finished
+    if notification[:action] == 'finished'
+      payload[:value1] = "#{user} has finished watching #{notification[:title]}."
+      RestClient.post(ifttt_url, payload.to_json, headers)
+      return
+    end
+
+    # hash of approved streaming IPs per user
+    approved_ips = {
+        'addietz55c': {
+            home: '98.220.134.109'
+        },
+        'dtmob48': {
+            home: '68.45.34.102'
+        },
+        'Rileyhopesuthy': {
+            home: '71.46.94.22'
+        },
+        'thejudeallen': {
+            home: '184.175.156.31'
+        },
+        'whartman': {
+            home: '73.22.188.222'
+        },
+        'crazynickmarshall': {},
+        'jordannicmarshall': {},
+        'paulfanfoot': {}
+    }
+
+    # matches a user's IP to a location
+    location = ''
+    approved_ips[user].each do |key, value|
+      if notification[:ipAddress] == value
+        location = key.to_s
+      end
+    end
+
+    # build and send IFTTT notification
+    payload[:value1] = "#{user} #{notification[:action]} watching #{notification[:title]}"
+    payload[:value2] = location.empty? ? ". Unknown IP #{notification[:ipAddress]} detected!" : " from #{location}."
+    payload[:value3] = " Stream count: #{notification[:activeStreams]}"
+    RestClient.post(ifttt_url, payload.to_json, headers)
+  end
+
+  ## REQUEST PAYLOAD RECEIVED FROM TAUTULLI ###
+  # {
+  #     "action": {action},
+  #     "activeStreams": {streams},
+  #     "user": {user},
+  #     "device": {device},
+  #     "platform": {platform},
+  #     "product": {product},
+  #     "player": {player},
+  #     "ipAddress": {ip_address},
+  #     "quality": {quality_profile},
+  #     "bandwidth": {stream_bandwidth},
+  #     "mediaType": {media_type},
+  #     "title": {title}
+  # }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  # smarthings routes
   root 'smartthings#root'
   get '/leaving', to: 'smartthings#leaving'
   post '/lifecycle', to: 'smartthings#lifecycle'
   get '/garage/on', to: 'smartthings#garage_on'
   get '/garage/off', to: 'smartthings#garage_off'
   get '/subscriptions/new', to: 'smartthings#new_subscription'
+
+  # plex routes
+  post '/plex', to: 'plex#notify'
 end
